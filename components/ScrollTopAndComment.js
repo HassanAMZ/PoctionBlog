4d4ec9101ddce23ece2894022f94c512ecd18d1b
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2021-10-24 23:14:58 +0500

    Removed Github link, Fixed Buttons z-index,Footer

--- components/ScrollTopAndComment.js
+++ components/ScrollTopAndComment.js
@@ -22,7 +22,7 @@ const ScrollTopAndComment = () => {
     document.getElementById('comment').scrollIntoView()
   }
   return (
-    <div className="fixed flex-col hidden gap-3 right-8 bottom-8 md:flex">
+    <div className="fixed flex-col hidden gap-3 right-8 bottom-8 md:flex z-10">
       <button
         aria-label="Scroll To Comment"
         type="button"
