b8d2d3249c00666e7a20a10236fcf5e00e3b8462
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2021-11-12 09:44:45 +0500

    Updated webinar pages

--- data/projectsData.js
+++ data/projectsData.js
@@ -4,7 +4,7 @@ const projectsData = [
     description: `Facebook CAPI to track iOS14+ user events such as FormSubmission, ThankYouPages on website, the events are deduplicated with an eventID and are sent back to the Facebook Event Manager. 
     
     The setup requires a cloud hosting for GTM server container.`,
-    imgSrc: '/static/images/FacebookCAPIServerTracking.png',
+    imgSrc: '/static/images/projectImages/FacebookCAPIServerTracking.png',
     href:
       'https://www.upwork.com/services/product/facebook-conversion-api-server-tracking-for-your-lead-generation-wesbite-1439356248607649792',
   },
@@ -13,7 +13,7 @@ const projectsData = [
     description: `Facebook CAPI to track iOS14+ user events such as AddToCart, BeginCheckout, Purchase on website, the events are deduplicated with an eventID and are sent back to the Facebook Event Manager. 
     
     The setup requires a cloud hosting for GTM server container.`,
-    imgSrc: '/static/images/GA4&FbPixelTrackingForEcommerceStore.png',
+    imgSrc: '/static/images/projectImages/GA4&FbPixelTrackingForEcommerceStore.png',
     href:
       'https://www.upwork.com/services/product/facebook-conversion-api-server-tracking-for-your-ecommerce-store-1395715167350927360',
   },
---
+++ pages/webinar.js
@@ -0,0 +1,37 @@
+import siteMetadata from '@/data/siteMetadata'
+import webinarData from '@/data/webinarData'
+import Card from '@/components/Card'
+import { PageSEO } from '@/components/SEO'
+
+export default function Webinar() {
+  return (
+    <>
+      <PageSEO title={`Webinars - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Webinars
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400"></p>
+        </div>
+        <div className="container py-12">
+          <div className="flex flex-wrap -m-4">
+            {webinarData.map((d) => (
+              <Card
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                imgSrc={d.imgSrc}
+                href={d.href}
+                date={d.date}
+                time={d.time}
+                host={d.host}
+                speaker={d.speaker}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
