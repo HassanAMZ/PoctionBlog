6516ce0a8243e3424ee15530fb09b1fe0be9981c
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2021-11-12 13:33:01 +0500

    Update webinarData.js

--- data/webinarData.js
+++ data/webinarData.js
@@ -1,6 +1,6 @@
 const webinarData = [
   {
-    title: 'Introduction To Freelancing 001',
+    title: 'Introduction To Freelancing 101',
     description: `Excited to announce the first ever zoom webinar, to get 1000 people free of their financial worries and help Azad Chaiwala in the mission of liberating 1 Million Pakistani from financial struggles.`,
     imgSrc: '/static/images/webinarImages/webinar15112021.png',
     href: '/webinars/15112021',
