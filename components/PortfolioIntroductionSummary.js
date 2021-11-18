3a6a057fa91bfa3e77821aabe8bff0616f07765c
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2021-11-18 15:29:36 +0500

    home image updated

--- components/PortfolioIntroductionSummary.js
+++ components/PortfolioIntroductionSummary.js
@@ -4,21 +4,27 @@ export default function PortfolioIntroductionSummary() {
   return (
     <>
       <div className="grid justify-items-center">
-        <div className="w-64 m-4 col-start-1 col-end-1 row-start-1 row-end-2 sm:col-start-1 sm:col-end-2 sm:row-start-2 sm:row-end-1">
+        <div className="w-72 m-4 col-start-1 col-end-1 row-start-1 row-end-2 sm:w-80 sm:col-start-1 sm:col-end-2 sm:row-start-2 sm:row-end-1">
           <Image
-            className="object-cover object-center lg:h-48 md:h-36"
             layout="responsive"
             height={1000}
             width={1000}
-            src="/static/images/logo.png"
+            src="/static/images/summaryPhoto.png"
             alt="Poction"
           />
+          <a href="https://www.freepik.com/vectors/creative">
+            Creative vector created by pikisuperstar - www.freepik.com
+          </a>
         </div>
-        <div className="m-0 col-start-1 col-end-1 row-start-2 row-end-3 sm:m-4 sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-2">
-          <h2 className="text-xl text-center sm:text-3xl sm:text-left">
-            Poction - Web & Server Analyst
-          </h2>
-          <span className="text-sm text-center sm:text-md sm:text-left">Plan, develop & Track</span>
+        <div className="m-0 text-center col-start-1 col-end-1 row-start-2 row-end-3 sm:text-left sm:m-4 sm:col-start-2 sm:col-end-3 sm:row-start-1 sm:row-end-2">
+          <div className="sm:mb-8">
+            <h2 className="text-xl text-center sm:text-3xl sm:text-left">
+              Poction - Web & Server Analyst
+            </h2>
+            <span className="text-sm text-center sm:text-md sm:text-left">
+              Plan, develop & Track
+            </span>
+          </div>
           <p className="text-lg text-center hidden sm:text-lg sm:text-left sm:block">
             Plan, develop and manage web tracking for Facebook, Google, Pinterest, HotJar and other
             analytics using Google Tag Manager Web and Server Containers.
