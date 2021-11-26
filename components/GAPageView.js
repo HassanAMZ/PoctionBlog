5a07f4de6277b8482b78ef99650cf0d58f07d5b0
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2021-11-26 22:11:36 +0500

    Added Eye icon for views

--- components/GAPageView.js
+++ components/GAPageView.js
@@ -1,4 +1,6 @@
 import useSWR from 'swr'
+import Eye from '../public/static/favicons/eye.svg'
+
 const GAPageView = ({ slug }) => {
   const { data } = useSWR(
     `/api/page-views?slug=/blog/${encodeURIComponent(slug)}`,
@@ -9,6 +11,13 @@ const GAPageView = ({ slug }) => {
     { revalidateOnFocus: false }
   )
   const views = data?.pageViews || 0
-  return <div className="text-gray-900 dark:text-gray-100"> Views {views}</div>
+  return (
+    <div xlassName="flex flex-row">
+      <>
+        <Eye />
+      </>
+      <div className="text-gray-900 dark:text-gray-100">{views}</div>
+    </div>
+  )
 }
 export default GAPageView
