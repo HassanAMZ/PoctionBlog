7d359cc3b3576f3537eead1b2238ca4fb07a15c2
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2021-11-30 18:38:50 +0500

    course blog added

---
+++ pages/course/page/[page].js
@@ -0,0 +1,56 @@
+import { PageSEO } from '@/components/SEO'
+import siteMetadata from '@/data/siteMetadata'
+import { getAllFilesFrontMatter } from '@/lib/mdx'
+import CourseListLayout from '@/layouts/CourseListLayout'
+import { POSTS_PER_PAGE } from '../../course'
+
+export async function getStaticPaths() {
+  const totalPosts = await getAllFilesFrontMatter('course')
+  const totalPages = Math.ceil(totalPosts.length / POSTS_PER_PAGE)
+  const paths = Array.from({ length: totalPages }, (_, i) => ({
+    params: { page: (i + 1).toString() },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps(context) {
+  const {
+    params: { page },
+  } = context
+  const posts = await getAllFilesFrontMatter('course')
+  const pageNumber = parseInt(page)
+  const initialDisplayPosts = posts.slice(
+    POSTS_PER_PAGE * (pageNumber - 1),
+    POSTS_PER_PAGE * pageNumber
+  )
+  const pagination = {
+    currentPage: pageNumber,
+    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+  }
+
+  return {
+    props: {
+      posts,
+      initialDisplayPosts,
+      pagination,
+    },
+  }
+}
+
+export default function PostPage({ posts, initialDisplayPosts, pagination }) {
+  return (
+    <>
+      <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <CourseListLayout
+        posts={posts}
+        initialDisplayPosts={initialDisplayPosts}
+        pagination={pagination}
+        title="All Courses"
+      />
+    </>
+  )
+}
