a0b9c363c9c8d866d18b7be6de116bfd923173ff
Shahzada Ali Hassan <reactjswebdev@gmail.com>
2022-01-02 05:08:07 +0500

    Added Tags on HomePage

--- pages/index.js
+++ pages/index.js
@@ -3,16 +3,17 @@ import { PageSEO } from '@/components/SEO'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
-import formatDate from '@/lib/utils/formatDate'
 import NewsletterForm from '@/components/NewsletterForm'
-import GAPageView from '@/components/GAPageView'
 import { useEffect } from 'react'
 import FeaturedPosts from '@/components/FeaturedPosts'
 import ListLayout from '@/layouts/ListLayout'
+import { getAllTags } from '@/lib/tags'
+import kebabCase from '@/lib/utils/kebabCase'
 
 export const POSTS_PER_PAGE = 5
 const MAX_DISPLAY = 5
 export async function getStaticProps() {
+  const tags = await getAllTags('blog')
   const posts = await getAllFilesFrontMatter('blog')
   const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE)
   const pagination = {
@@ -20,10 +21,10 @@ export async function getStaticProps() {
     totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
   }
 
-  return { props: { initialDisplayPosts, posts, pagination } }
+  return { props: { initialDisplayPosts, posts, pagination, tags } }
 }
 
-export default function Home({ posts, initialDisplayPosts, pagination }) {
+export default function Home({ posts, initialDisplayPosts, pagination, tags }) {
   useEffect(() => {
     const [
       allBlogPostIDs,
@@ -64,6 +65,7 @@ export default function Home({ posts, initialDisplayPosts, pagination }) {
       },
     })
   }, [])
+  const sortedTags = Object.keys(tags).sort((a, b) => tags[b] - tags[a])
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
@@ -73,7 +75,7 @@ export default function Home({ posts, initialDisplayPosts, pagination }) {
         posts={posts}
         initialDisplayPosts={initialDisplayPosts}
         pagination={pagination}
-        title="All Posts"
+        title="Recent Posts"
       />
 
       {posts.length > MAX_DISPLAY && (
@@ -87,6 +89,48 @@ export default function Home({ posts, initialDisplayPosts, pagination }) {
           </Link>
         </div>
       )}
+      <>
+        <div className="flex flex-col justify-left my-4">
+          <div className="py-3 space-x-2 md:space-y-5">
+            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+              Top Tags
+            </h1>
+          </div>
+          <div className="flex flex-wrap max-w-3xl">
+            {Object.keys(tags).length === 0 && 'No tags found.'}
+            {sortedTags.map((t) => {
+              if (tags[t] > 3) {
+                return (
+                  <>
+                    <div key={t} className="my-1 sm:my-2 mr-5">
+                      <Tag text={t} />
+                      <Link
+                        href={`/tags/${kebabCase(t)}`}
+                        className="-ml-2 text-sm font-semibold text-gray-600 uppercase dark:text-gray-300"
+                      >
+                        {` (${tags[t]})`}
+                      </Link>
+                    </div>
+                  </>
+                )
+              } else {
+                return <></>
+              }
+            })}
+          </div>
+        </div>
+        {
+          <div className="flex justify-end text-base font-medium leading-6">
+            <Link
+              href="/tags"
+              className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+              aria-label="all tags"
+            >
+              All Tags &rarr;
+            </Link>
+          </div>
+        }
+      </>
       {siteMetadata.newsletter.provider !== '' && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
